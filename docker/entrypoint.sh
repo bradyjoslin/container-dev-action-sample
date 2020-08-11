1d254022d6f3c73da1bdf6615227a19512ab0682
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-08-11 15:25:24 +0000

    simplify

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,37 +2,11 @@
 
 set -e
 
+# Check if running in GitHub vs locally
 if [ -n "$GITHUB_ACTIONS" ]
 then
-
-  # Used to execute any specified pre and post commands
-  execute_commands() {
-    COMMANDS=$1
-    while IFS= read -r COMMAND; do
-      CHUNKS=()
-
-      for CHUNK in $COMMAND; do
-        CHUNKS+=("$CHUNK")
-      done
-
-      "${CHUNKS[@]}"
-
-      CHUNKS=()
-    done <<< "$COMMANDS"
-  }
-
-  # If precommands is detected as input
-  if [ -n "$INPUT_PRECOMMANDS" ]
-  then
-    execute_commands "$INPUT_PRECOMMANDS"
-  fi
-
-  echo "Something awesome happens here!"
-
-  # If postcommands is detected as input
-  if [ -n "$INPUT_POSTCOMMANDS" ]
-  then
-    execute_commands "$INPUT_POSTCOMMANDS"
-  fi
-
+  echo "** Running github action script **"
+  npm run hello:jq
+  npm run hello:cat
+  echo "** **"
 fi
