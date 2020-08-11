8b05148329c4f7ad8c329bb06c42e935bcc071f8
Joslin, Brady W (Brady) <brady@bradyjoslin.com>
2020-08-11 04:39:36 +0000

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# devcontainer fun
+
+An experiment for sharing a dockerfile for both local development using VS Code devcontainers and for CI/CD with GitHub Actions.
---
+++ docker/entrypoint.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+
+if [ -n "$GITHUB_ACTIONS" ]
+then
+
+  # Used to execute any specified pre and post commands
+  execute_commands() {
+    COMMANDS=$1
+    while IFS= read -r COMMAND; do
+      CHUNKS=()
+
+      for CHUNK in $COMMAND; do
+        CHUNKS+=("$CHUNK")
+      done
+
+      "${CHUNKS[@]}"
+
+      CHUNKS=()
+    done <<< "$COMMANDS"
+  }
+
+  # If precommands is detected as input
+  if [ -n "$INPUT_PRECOMMANDS" ]
+  then
+    execute_commands "$INPUT_PRECOMMANDS"
+  fi
+
+  echo "Something awesome happens here!"
+
+  # If postcommands is detected as input
+  if [ -n "$INPUT_POSTCOMMANDS" ]
+  then
+    execute_commands "$INPUT_POSTCOMMANDS"
+  fi
+
+fi
